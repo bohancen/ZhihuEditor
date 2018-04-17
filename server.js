41e2d4b67c28f28b1f48fd38aaef46dc5babb461
Daubert <jzh16s@hotmail.com>
2018-04-17 13:45:37 +0800

    提交源码

---
+++ server.js
@@ -0,0 +1,23 @@
+const http = require("http")
+const url = require("url")
+const path = require("path")
+const fs = require("fs")
+const port = process.argv[2] || 8888
+
+http.createServer((request, response) => {
+    const uri = url.parse(request.url).pathname
+    const fileName = path.join(process.cwd(), uri, '/index.html')
+    fs.readFile(fileName, "binary", (err, file) => {
+        if(err) {
+            response.writeHead(500, {"Content-Type": "text/plain"})
+            response.write(err + "\n")
+            response.end()
+            return
+        }
+        response.writeHead(200)
+        response.write(file, "binary")
+        response.end()
+    })
+}).listen(parseInt(port, 10))
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown")
